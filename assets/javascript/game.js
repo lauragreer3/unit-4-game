ac9c6b31f84146e60e62322e58364cdff5ab4232
Moonkitten <lauragreer3@gmail.com>
2019-01-19 23:25:46 -0700

    html structure created

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//4 buttons, 1 for each crystal
+//create random number for target score between 19-120
+//create random number for each crystal 1-12
+//create variable to track user score
+//create variable for wins, losses
+//create variable for gamestate
+
